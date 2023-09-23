634b36b3bd0f2b15fe866c9812d48e5a1fb93e11
lucascampos04 <camposdlucasoli@gmail.com>
2023-09-23 14:48:59 -0300

    Pagina de manuntenção

---
+++ src/main/java/com/colegio/jorgina/colegiojorginasite/ColegioJorginaSiteApplication.java
@@ -0,0 +1,13 @@
+package com.colegio.jorgina.colegiojorginasite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColegioJorginaSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColegioJorginaSiteApplication.class, args);
+    }
+
+}
